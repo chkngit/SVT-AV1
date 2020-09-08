d98ebfb900b4b25a18092d19696ecacfef7d6ab3
hguermaz <hassen.guermazi@intel.com>
2020-09-08 11:46:18 -0700

    svt-03-v4

--- Source/Lib/Encoder/Codec/EbEncDecProcess.c
+++ Source/Lib/Encoder/Codec/EbEncDecProcess.c
@@ -1729,7 +1729,11 @@ void set_block_based_depth_refinement_controls(ModeDecisionContext *mdctxt, uint
         break;
     case 5:
         depth_refinement_ctrls->enabled = 1;
+#if PARENT_COST
+        depth_refinement_ctrls->parent_to_current_th = 5;
+#else
         depth_refinement_ctrls->parent_to_current_th = -10;
+#endif
         depth_refinement_ctrls->sub_to_current_th = 5;
         depth_refinement_ctrls->use_pred_block_cost = 1;
         break;
