4fcb875b005886dbd53678cdd9641b97472ef079
hguermaz <hassen.guermazi@intel.com>
2020-09-14 23:41:03 -0700

    NEW_TXT

--- Source/API/EbDebugMacros.h
+++ Source/API/EbDebugMacros.h
@@ -63,7 +63,7 @@ extern "C" {
 #define REMOVE_TXT_STATS 1
 #define TX_TYPE_GROUPING 1
 
-#define COST_BASED_TXT 1
+#define COST_BASED_TXT 0
 #define PREVIOUS_GROUP_EXIT 0
 /************************************/
 #define TEST_BLOCK_BASED 0
