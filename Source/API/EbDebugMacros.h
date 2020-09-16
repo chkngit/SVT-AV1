f61de19e12fa6d3256b90fe30e784d4c9beeea99
hguermaz <hassen.guermazi@intel.com>
2020-09-16 15:12:25 -0700

    CHECK_CHROMA

--- Source/API/EbDebugMacros.h
+++ Source/API/EbDebugMacros.h
@@ -72,7 +72,7 @@ extern "C" {
 #define SHUT_RDOQ 0 
 #define SHUT_FP_QUANT 0 
 #define SHUT_FP_QUANT_TX_SIZE 0
-#define SHUT_FP_QUANT_CHROMA 0 //--->
+#define SHUT_FP_QUANT_CHROMA 1 //--->
 #define SHUT_FP_QUANT_TX_TYPE 0
 #define SHUT_FP_QUANT_INTER 0 
 #define SHUT_FP_QUANT_INTRA 0
@@ -80,7 +80,7 @@ extern "C" {
 #define FAST_RDOQ 0 
 #define FAST_RDOQ_INTER 0
 #define FAST_RDOQ_EOB 0
-#define FAST_RDOQ_CHROMA 0 //--->
+#define FAST_RDOQ_CHROMA 1 //--->
 #define FAST_RDOQ_TH 0
 /************************************/
 #ifdef __cplusplus
