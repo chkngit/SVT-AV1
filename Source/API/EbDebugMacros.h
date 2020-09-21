7c358577b926420197893f3286f2e20aca123cae
hguermaz <hassen.guermazi@intel.com>
2020-09-21 11:50:38 -0700

    SHUT_RDOQ

--- Source/API/EbDebugMacros.h
+++ Source/API/EbDebugMacros.h
@@ -73,7 +73,7 @@ extern "C" {
 #define TEST_BLOCK_BASED 0
 /************************************/
 #define SHUT_RDOQ_CHROMA 0 
-#define SHUT_RDOQ 0 
+#define SHUT_RDOQ 1 
 #define SHUT_FP_QUANT 0 
 #define SHUT_FP_QUANT_TX_SIZE 0
 #define SHUT_FP_QUANT_TX_TYPE 0
