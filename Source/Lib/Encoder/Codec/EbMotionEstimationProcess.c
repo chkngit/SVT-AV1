b6e161b7d09f48d82075443112dc49d2df81c0fe
hguermaz <hassen.guermazi@intel.com>
2020-09-22 16:52:26 -0700

    HME_LEVEL_2_8_3

--- Source/Lib/Encoder/Codec/EbMotionEstimationProcess.c
+++ Source/Lib/Encoder/Codec/EbMotionEstimationProcess.c
@@ -189,12 +189,17 @@ void *set_me_hme_params_oq(MeContext *me_context_ptr, PictureParentControlSet *p
         me_context_ptr->hme_level1_search_area_in_height_array[1] = 16;
 
 
+
+#if HME_LEVEL_2
+    me_context_ptr->hme_level2_search_area_in_width_array[0] =
+        me_context_ptr->hme_level2_search_area_in_width_array[1] = 8;
+
+    me_context_ptr->hme_level2_search_area_in_height_array[0] =
+        me_context_ptr->hme_level2_search_area_in_height_array[1] = 3;
+#else
     me_context_ptr->hme_level2_search_area_in_width_array[0] =
         me_context_ptr->hme_level2_search_area_in_width_array[1] =
         me_context_ptr->hme_level2_search_area_in_height_array[0] =
-#if HME_LEVEL_2
-        me_context_ptr->hme_level2_search_area_in_height_array[1] = 8;
-#else
         me_context_ptr->hme_level2_search_area_in_height_array[1] = 16;
 #endif
     if (!pcs_ptr->sc_content_detected)
