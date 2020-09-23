bfb707206b5f988a8f08beef9145754b792028cb
hguermaz <hassen.guermazi@intel.com>
2020-09-23 15:10:12 -0700

    rate_equal_2

--- Source/Lib/Encoder/Codec/EbEncDecProcess.c
+++ Source/Lib/Encoder/Codec/EbEncDecProcess.c
@@ -4164,7 +4164,7 @@ static void build_starting_cand_block_array(SequenceControlSet *scs_ptr, Picture
     else {
         uint64_t cost = RDCOST(fast_lambda, 0, pcs_ptr->parent_pcs_ptr->rc_me_distortion[sb_index]);
         //uint64_t cost_th = (64 * 64);// th0 RDCOST(fast_lambda, 8, 64 * 64);
-        uint64_t cost_th = RDCOST(fast_lambda, 1, 64 * 64);
+        uint64_t cost_th = RDCOST(fast_lambda, 2, 64 * 64);
         //uint64_t cost_th = (3 * 64 * 64);// th2 RDCOST(fast_lambda, 8, 64 * 64);
 
         min_sq_size = (cost < cost_th) ?
