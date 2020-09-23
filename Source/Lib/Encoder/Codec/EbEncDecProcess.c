ef25715ce13fb7b7adafca6b08b557d6b4809711
hguermaz <hassen.guermazi@intel.com>
2020-09-23 10:14:11 -0700

    MD_EXIT_25_PD0

--- Source/Lib/Encoder/Codec/EbEncDecProcess.c
+++ Source/Lib/Encoder/Codec/EbEncDecProcess.c
@@ -2326,7 +2326,7 @@ EbErrorType signal_derivation_enc_dec_kernel_oq(
 #if PD_MD_EXIT
     // Derive MD Exit TH
     if (pd_pass == PD_PASS_0)
-        context_ptr->md_exit_th = 0;
+        context_ptr->md_exit_th = 25;
     else if (pd_pass == PD_PASS_1)
         context_ptr->md_exit_th = 0;
     else
