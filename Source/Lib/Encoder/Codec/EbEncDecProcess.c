5133a218468230c8e22fb927970cd221760658fc
hguermaz <hassen.guermazi@intel.com>
2020-09-23 11:05:21 -0700

    MD_EXIT_0

--- Source/Lib/Encoder/Codec/EbEncDecProcess.c
+++ Source/Lib/Encoder/Codec/EbEncDecProcess.c
@@ -2330,7 +2330,7 @@ EbErrorType signal_derivation_enc_dec_kernel_oq(
     else if (pd_pass == PD_PASS_1)
         context_ptr->md_exit_th = 0;
     else
-        context_ptr->md_exit_th = 50;
+        context_ptr->md_exit_th = 0;
 #endif
     if (pd_pass == PD_PASS_0)
         context_ptr->interpolation_search_level = IFS_OFF;
