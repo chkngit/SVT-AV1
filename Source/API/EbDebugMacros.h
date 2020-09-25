eb2daafc6a3d3ea7223341fa2ce71af1cb307188
hguermaz <hassen.guermazi@intel.com>
2020-09-25 15:50:29 -0700

    TUNE_PME

--- Source/API/EbDebugMacros.h
+++ Source/API/EbDebugMacros.h
@@ -42,7 +42,7 @@ extern "C" {
 #if 1 // ALL_OFF
 #if 1 // ONLY_LOSSLESS
 /************************************/
-#define TUNE_PME 0
+#define TUNE_PME 1
 #define TUNE_SUBPEL 0
 #define SHUT_MV_COST 0
 #define TUNE_REF 0
