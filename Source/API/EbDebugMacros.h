dd6074fcc204065ceb4001a901c407553fca122b
hguermaz <hassen.guermazi@intel.com>
2020-09-25 10:49:21 -0700

    #if 0 // just double check

--- Source/API/EbDebugMacros.h
+++ Source/API/EbDebugMacros.h
@@ -186,9 +186,12 @@ extern "C" {
 #define ON_THE_FLY_MDS1_BYPASS 0
 #endif
 
+#if 0 // just double check
 #define ADD_GM_TO_M8 1 // Add GM to M7; change CFL and TF settings to offset speed loss
 #define FEATURE_SKIP_GM_UNIPRED 1 // Add ability to skip GM unipred injection, and use bipred only
 #define CDEF_OPT                1 // Added new fast search for CDEF
+#endif
+
 #ifdef __cplusplus
 }
 #endif // __cplusplus
